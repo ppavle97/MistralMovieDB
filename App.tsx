34fa58e145fa9d27f211ffa62149e29a1e5fd501
ppavle97 <ppavle@live.com>
2022-01-29 12:45:11 +0100

    Create navigation component

--- App.tsx
+++ App.tsx
@@ -1,44 +1,6 @@
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { Details, Movies, TvShow } from "./src/screens";
-
-const Tabs = createBottomTabNavigator();
-const MoviesStack = createStackNavigator();
-const TvShowStack = createStackNavigator();
-
-const MoviesStackScreen = () => (
-  <MoviesStack.Navigator>
-    <MoviesStack.Screen name="Movies" component={Movies} />
-    <MoviesStack.Screen name="Details" component={Details} />
-  </MoviesStack.Navigator>
-);
-
-const TvShowStackScreen = () => (
-  <TvShowStack.Navigator>
-    <TvShowStack.Screen name="TvShow" component={TvShow} />
-    <MoviesStack.Screen name="Details" component={Details} />
-  </TvShowStack.Navigator>
-);
+import Navigation from "./src/navigation/Navigation";
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Tabs.Navigator
-        screenOptions={{
-          tabBarLabelPosition: "beside-icon",
-          tabBarLabelStyle: {
-            fontWeight: "700",
-            fontSize: 15,
-          },
-          tabBarIconStyle: { display: "none" },
-          headerShown: false,
-        }}
-      >
-        <Tabs.Screen name="Movies" component={MoviesStackScreen} />
-        <Tabs.Screen name="TvShows" component={TvShowStackScreen} />
-      </Tabs.Navigator>
-    </NavigationContainer>
-  );
+  return <Navigation />;
 }
---
+++ src/navigation/Navigation.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Details, Movies, TvShow } from "../screens";
+
+const Tabs = createBottomTabNavigator();
+const MoviesStack = createStackNavigator();
+const TvShowStack = createStackNavigator();
+
+const MoviesStackScreen = () => (
+  <MoviesStack.Navigator>
+    <MoviesStack.Screen name="Movies" component={Movies} />
+    <MoviesStack.Screen name="Details" component={Details} />
+  </MoviesStack.Navigator>
+);
+
+const TvShowStackScreen = () => (
+  <TvShowStack.Navigator>
+    <TvShowStack.Screen name="TvShow" component={TvShow} />
+    <MoviesStack.Screen name="Details" component={Details} />
+  </TvShowStack.Navigator>
+);
+
+export default function Navigation() {
+  return (
+    <NavigationContainer>
+      <Tabs.Navigator
+        screenOptions={{
+          tabBarLabelStyle: {
+            fontWeight: "700",
+            fontSize: 15,
+          },
+          tabBarIconStyle: { display: "none" },
+          headerShown: false,
+        }}
+      >
+        <Tabs.Screen name="Movies" component={MoviesStackScreen} />
+        <Tabs.Screen name="TvShows" component={TvShowStackScreen} />
+      </Tabs.Navigator>
+    </NavigationContainer>
+  );
+}
